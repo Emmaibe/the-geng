d62d4d95c597231673a14d937519188dde42c2f0
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-05-24 15:07:23 +0100

    profile-setup

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const button = document.querySelectorAll("button");
+const detail = document.querySelector(".details-cont");
+const profileImg = document.querySelector(".profile-img");
+const profile = document.querySelector(".profile");
 
 button.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -8,4 +11,9 @@ button.forEach((btn) => {
       btn.removeAttribute("btn-focus");
     }, 200);
   });
+});
+
+profile.addEventListener("click", () => {
+  detail.toggleAttribute("visible");
+  // profileImg.style.opacity = "0";
 });
